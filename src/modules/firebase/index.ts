adef46afa96c4f93dee1b1c240f3abe3809d7421
alejandro <caicedorivera1999@hotmail.com>
2026-06-25 14:34:28 -0500

    feat(firebase): add authentication service export

--- src/modules/firebase/index.ts
+++ src/modules/firebase/index.ts
@@ -1,2 +1,3 @@
 export { default as firestoreService } from './infrastructure/firestore.service';
 export { default as storageService } from './infrastructure/storage.service';
+export { default as authenticationService } from './infrastructure/authentication.service';
